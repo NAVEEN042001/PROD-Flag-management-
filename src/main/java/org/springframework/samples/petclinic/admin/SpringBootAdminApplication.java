3aa7486b3237ad4af6ac9b1e090b83a96fac1e0d
dhina <dhina.s@logicfocus.net>
2024-10-07 17:53:03 +0530

    Flags

--- src/main/java/org/springframework/samples/petclinic/admin/SpringBootAdminApplication.java
+++ src/main/java/org/springframework/samples/petclinic/admin/SpringBootAdminApplication.java
@@ -16,15 +16,62 @@
 package org.springframework.samples.petclinic.admin;
 
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import io.rollout.client.ConfigurationFetchedHandler;
+import io.rollout.client.FetcherResults;
+import io.rollout.client.FetcherStatus;
+import io.rollout.rox.server.Rox;
+import io.rollout.rox.server.RoxOptions;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+import java.util.concurrent.ExecutionException;
 
 @SpringBootApplication
 @EnableAdminServer
 @EnableDiscoveryClient
 public class SpringBootAdminApplication {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        Flags flags = new Flags();
+        // Register the flags container with Rollout
+        Rox.register(flags);
+        // Building options
+        RoxOptions options = new RoxOptions.Builder()
+                .withConfigurationFetchedHandler(new ConfigurationFetchedHandler() {
+                    @Override
+                    public void onConfigurationFetched(FetcherResults fetcherResults) {
+                        if (fetcherResults != null) {
+                            FetcherStatus status = fetcherResults.getFetcherStatus();
+                            // configuration loaded from network, flags value updated
+                            if (status != null && status == FetcherStatus.AppliedFromNetwork) {
+                                System.out.println("flags value updated");
+                            }
+                        }
+                    }
+                })
+                .build();
+        // Setup the Rollout environment key
+        Rox.setup("66fe9bb334863653de479a9e", options).get();
+        // Boolean flag example
+        if (flags.enableTutorial.isEnabled()) {
+            // TODO:  Put your code here that needs to be gated
+        }
+        // String flag example
+        String titleColor = flags.titleColors.getValue();
+        System.out.printf("Title color is %s%n", titleColor);
+
+// Integer flag example
+        int titleSize = flags.titleSize.getValue();
+        System.out.printf("Title size is %d%n", titleSize);
+
+// Double flag example
+        double specialNumber = flags.specialNumber.getValue();
+        System.out.printf("Special number is %.2f%n", specialNumber);
+
+// Enum flag example
+        Flags.Color color = flags.titleColorsEnum.getValue();
+        System.out.printf("Enum color is %s%n", color.name());
+
         SpringApplication.run(SpringBootAdminApplication.class, args);
     }
 
